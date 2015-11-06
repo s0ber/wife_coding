8d72af7460cac271c32024dc72ec68a0b8501333
Sergey <sergeyshishkalov@gmail.com>
2015-11-06 22:26:17 +0300

    Move to sass

---
+++ chapter5.8/gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var jade = require('gulp-jade');
+
+gulp.task('compile_html', function() {
+  gulp.src('./src/**/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('./build'))
+});
+
+gulp.task('compile_css', function () {
+  gulp.src('./src/styles/main.sass')
+    .pipe(sass({includePaths: ['./src/styles']}).on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions', '> 1%', 'ie >= 8']
+    }))
+    .pipe(gulp.dest('./build/styles'));
+});
+
+gulp.task('watch', ['compile_html', 'compile_css'], function () {
+  gulp.watch(['./src/styles/**/*.scss', './src/styles/**/*.sass'], ['compile_css']);
+  gulp.watch('./src/**/*.jade', ['compile_html']);
+});
